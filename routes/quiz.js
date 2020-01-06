05ceed23ec122356500ac0cb0373c6fc0c4a178c
Krzysztof Baum <baumkrzysztof@gmail.com>
2020-01-06 16:13:36 +0100

    quiz logic

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const Quiz = require("../models/quiz");
 
 /* GET home page. */
 router.get("/", (req, res) => {
-  res.render("index", { title: "Quiz" });
+  const show = !req.session.votes;
+
+  Quiz.find({}, (err, data) => {
+    let summ = 0;
+    data.forEach(item => {
+      summ += item.votes;
+    });
+
+    res.render("quiz", { title: "Quiz", data, show, summ });
+  });
+});
+router.post("/", (req, res) => {
+  const id = req.body.quiz;
+  Quiz.findOne({ _id: id }, (err, data) => {
+    data.votes++;
+    data.save(() => {
+      req.session.votes = 1;
+      res.redirect("/quiz");
+    });
+  });
 });
 
 module.exports = router;
