0c6602373675bb3bf80554f8e7a19ec26a8cdcec
Krzysztof Baum <baumkrzysztof@gmail.com>
2020-01-06 14:12:52 +0100

    admin and news forms

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  keySession: ["falafel"],
+  maxAgeSession: 24 * 60 * 60 * 1000,
+  db:
+    "mongodb+srv://Krzysztof:burgundia@cluster0-q60tt.mongodb.net/test?retryWrites=true&w=majority"
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,37 @@
 const express = require("express");
 const router = express.Router();
-
-/* GET home page. */
+const News = require("../models/news");
+router.all("*", (req, res, next) => {
+  if (req.session.admin !== 1) {
+    res.redirect("/login");
+    return;
+  }
+  next();
+});
 router.get("/", (req, res) => {
-  res.render("index", { title: "Admin" });
+  // console.log(req.session.admin);
+  const data = News.find({}, (err, data) => {
+    console.log(data);
+    res.render("admin/index", { title: "Admin", data });
+  });
+});
+router.get("/news/add", (req, res) => {
+  res.render("admin/newsForm", { title: "Dodaj newsa", errors: {} });
+});
+router.get("/news/delete/:id", (req, res) => {
+  News.findByIdAndDelete(req.params.id, err => {
+    res.redirect("/");
+  });
 });
 
+router.post("/news/add", (req, res) => {
+  const newsData = new News(req.body);
+  const errors = newsData.validateSync();
+  newsData.save();
+  console.log(errors);
+  if (errors !== []) {
+    res.redirect("/");
+  }
+  res.render("admin/newsForm", { title: "dodaj newsa", errors });
+});
 module.exports = router;
